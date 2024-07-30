db0c611fc816c5420aea58ed4c524ee7c04747b2
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-07-30 13:04:58 +0300

    Saving token in local storage.

--- src/store/Reducers/authReducer.js
+++ src/store/Reducers/authReducer.js
@@ -16,6 +16,7 @@ export const admin_login = createAsyncThunk(
       const { data } = await api.post('/admin-login', info, {
         withCredentials: true,
       });
+      localStorage.setItem('accessToken', data.token);
       // console.log(data);
       return fulfillWithValue(data);
     } catch (error) {
