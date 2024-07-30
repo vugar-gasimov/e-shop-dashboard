41ddee6a30c95c43a2d05c412324441abbcfd9f1
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-07-30 14:24:23 +0300

    Routes and layout, admin dashboad and home route and page.

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const MainLayout = () => {
+  return (
+    <div>
+      <h1>Main Layout</h1>
+      <Outlet />
+    </div>
+  );
+};
+
+export default MainLayout;
---
+++ src/layout/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <div>
+      <h2>My Sidebar</h2>
+    </div>
+  );
+};
+
+export default Sidebar;
