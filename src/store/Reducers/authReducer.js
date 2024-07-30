534752846f113d7fee549105dd6b17997a4d8c00
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-07-30 03:51:15 +0300

    Admin login error message with react hot toast and state update plus cleaning messages.

--- src/store/Reducers/authReducer.js
+++ src/store/Reducers/authReducer.js
@@ -10,15 +10,17 @@ const initialState = {
 
 export const admin_login = createAsyncThunk(
   'auth/admin_login',
-  async (info) => {
+  async (info, { rejectWithValue, fulfillWithValue }) => {
     console.log(info);
     try {
       const { data } = await api.post('/admin-login', info, {
         withCredentials: true,
       });
-      console.log(data);
+      // console.log(data);
+      return fulfillWithValue(data);
     } catch (error) {
-      console.log(error.response?.data);
+      return rejectWithValue(error.response?.data);
+      // console.log(error.response?.data);
     }
   }
 );
@@ -26,14 +28,30 @@ export const admin_login = createAsyncThunk(
 export const authReducer = createSlice({
   name: 'auth',
   initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder.addCase(admin_login.pending, (state, { payload }) => {
-      state.loader = true;
+  reducers: {
+    clearMessages: (state, payload) => {
       state.successMessage = '';
       state.errorMessage = '';
-    });
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(admin_login.pending, (state, { payload }) => {
+        state.loader = true;
+        state.successMessage = '';
+        state.errorMessage = '';
+      })
+      // .addCase(admin_login.fulfilled, (state, action) => {
+      //   state.loader = false;
+      //   state.userInfo = action.payload.user;
+      //   state.successMessage = 'Login successful!';
+      // })
+      .addCase(admin_login.rejected, (state, { payload }) => {
+        state.loader = false;
+        state.errorMessage = payload.error || 'Login failed.';
+      });
   },
 });
 
+export const { clearMessages } = authReducer.actions;
 export default authReducer.reducer;
