d064d6f66787859750944259d299f4a153678850
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-07-30 15:02:10 +0300

    Beginning of styling sidebar.

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
     <div>
-      <h2>My Sidebar</h2>
+      <div></div>
+      <div
+        className={`w-[260px] fixed bg-[#e6e7fb] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all`}
+      >
+        <div className='h-[70px] flex justify-center items-center'>
+          <Link className='w-[180px] h-[50px]' to='/'>
+            <img
+              className='w-full h-full'
+              src='http://localhost:3000/images/logo.png'
+              alt='Website logo.'
+            />
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
