d026be90386410a4e603b0446451dd6f067d6df8
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-08-18 22:37:38 +0400

    New reducer get user info and few changes, accepting role and access token from local storage, returning decoded role, id and checking expire time.

--- src/store/Reducers/authReducer.js
+++ src/store/Reducers/authReducer.js
@@ -1,12 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import api from '../../api/api';
-
-const initialState = {
-  successMessage: '',
-  errorMessage: '',
-  loader: false,
-  userInfo: null,
-};
+import { jwtDecode } from 'jwt-decode';
 
 export const admin_login = createAsyncThunk(
   'auth/admin_login',
@@ -31,8 +25,23 @@ export const vendor_login = createAsyncThunk(
       const { data } = await api.post('/vendor-login', info, {
         withCredentials: true,
       });
+
       localStorage.setItem('accessToken', data.token);
 
+      return fulfillWithValue(data);
+    } catch (error) {
+      return rejectWithValue(error.response?.data);
+    }
+  }
+);
+
+export const get_user_info = createAsyncThunk(
+  'auth/get_user_info',
+  async (_, { rejectWithValue, fulfillWithValue }) => {
+    try {
+      const { data } = await api.get('/get-user-info', {
+        withCredentials: true,
+      });
       return fulfillWithValue(data);
     } catch (error) {
       return rejectWithValue(error.response?.data);
@@ -58,11 +67,35 @@ export const vendor_register = createAsyncThunk(
   }
 );
 
+const returnRole = (token) => {
+  if (token) {
+    const decodeToken = jwtDecode(token);
+    const expireTime = new Date(decodeToken.exp * 1000);
+    if (new Date() > expireTime) {
+      localStorage.removeItem('accessToken');
+      return null;
+    } else {
+      return decodeToken.role;
+    }
+  } else {
+    return null;
+  }
+};
+
+const initialState = {
+  successMessage: '',
+  errorMessage: '',
+  loader: false,
+  userInfo: null,
+  role: returnRole(localStorage.getItem('accessToken')),
+  token: localStorage.getItem('accessToken'),
+};
+
 export const authReducer = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    clearMessages: (state, payload) => {
+    clearMessages: (state, _) => {
       state.successMessage = '';
       state.errorMessage = '';
     },
@@ -71,8 +104,6 @@ export const authReducer = createSlice({
     builder
       .addCase(admin_login.pending, (state, { payload }) => {
         state.loader = true;
-        // state.successMessage = '';
-        // state.errorMessage = '';
       })
       .addCase(admin_login.rejected, (state, { payload }) => {
         state.loader = false;
@@ -81,12 +112,24 @@ export const authReducer = createSlice({
       .addCase(admin_login.fulfilled, (state, { payload }) => {
         state.loader = false;
         state.successMessage = payload.message || 'Login successful!';
-        state.userInfo = payload.user;
+        state.token = payload.token;
+        state.role = returnRole(payload.token);
+      })
+      .addCase(vendor_login.pending, (state, { payload }) => {
+        state.loader = true;
+      })
+      .addCase(vendor_login.rejected, (state, { payload }) => {
+        state.loader = false;
+        state.errorMessage = payload.error || 'Login failed.';
+      })
+      .addCase(vendor_login.fulfilled, (state, { payload }) => {
+        state.loader = false;
+        state.successMessage = payload.message || 'Login successful!';
+        state.token = payload.token;
+        state.role = returnRole(payload.token);
       })
       .addCase(vendor_register.pending, (state, { payload }) => {
         state.loader = true;
-        // state.successMessage = '';
-        // state.errorMessage = '';
       })
       .addCase(vendor_register.rejected, (state, { payload }) => {
         state.loader = false;
@@ -95,7 +138,13 @@ export const authReducer = createSlice({
       .addCase(vendor_register.fulfilled, (state, { payload }) => {
         state.loader = false;
         state.successMessage = payload.message || 'Login successful!';
-        state.userInfo = payload.user;
+        state.token = payload.token;
+        state.role = returnRole(payload.token);
+      })
+      .addCase(get_user_info.fulfilled, (state, { payload }) => {
+        state.loader = false;
+        state.successMessage = payload.message || 'Login successful!';
+        state.userInfo = payload.userInfo;
       });
   },
 });
