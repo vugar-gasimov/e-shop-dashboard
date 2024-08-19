368f13aefaa2c73e0c507d1b623c1984dd02e797
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-08-19 21:09:26 +0400

    New file Protected routes for protecting admin and user urls.

--- src/router/routes/index.js
+++ src/router/routes/index.js
@@ -1,7 +1,12 @@
 import MainLayout from '../../layout/MainLayout';
 import { privateRoutes } from './privateRoutes';
+import ProtectedRoutes from './ProtectedRoutes';
 
 export const getRoutes = () => {
+  privateRoutes.map((r) => {
+    r.element = <ProtectedRoutes route={r}>{r.element}</ProtectedRoutes>;
+  });
+
   return {
     path: '/',
     element: <MainLayout />,
--- src/router/routes/publicRoutes.js
+++ src/router/routes/publicRoutes.js
@@ -4,6 +4,7 @@ const Login = lazy(() => import('../../views/auth/Login'));
 const Register = lazy(() => import('../../views/auth/Register'));
 const AdminLogin = lazy(() => import('../../views/auth/AdminLogin'));
 const Home = lazy(() => import('../../views/Home'));
+const Unauthorized = lazy(() => import('../../views/Unauthorized'));
 
 const publicRoutes = [
   {
@@ -21,6 +22,10 @@ const publicRoutes = [
   {
     path: '/admin/login',
     element: <AdminLogin />,
+  },
+  {
+    path: '/unauthorized',
+    element: <Unauthorized />,
   },
 ];
 export default publicRoutes;
