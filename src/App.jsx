ff2070b49547af1b1175ee1f56b1e17af36d0218
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-08-19 20:07:59 +0400

    Update get user info if there is token.

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@ import { useEffect, useState } from 'react';
 import Router from './router/Router';
 import publicRoutes from './router/routes/publicRoutes';
 import { getRoutes } from './router/routes';
+import { useDispatch, useSelector } from 'react-redux';
+import { get_user_info } from './store/Reducers/authReducer';
 
 function App() {
+  const dispatch = useDispatch();
+  const { token } = useSelector((state) => state.auth);
+
   const [allRoutes, setAllRoutes] = useState([...publicRoutes]);
 
   useEffect(() => {
     const routes = getRoutes();
     setAllRoutes([...allRoutes, routes]);
   }, []);
+
+  useEffect(() => {
+    if (token) {
+      dispatch(get_user_info());
+    }
+  }, [dispatch, token]);
 
   return <Router allRoutes={allRoutes} />;
 }
