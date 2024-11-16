769d79c7c46f48613433a5f57948e3a3aa99ba20
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-11-16 16:25:08 +0400

    Improving Banner Reducer from repeating and new reducers get and update banner, and thier add cases. Add banner page dispatch and show banners, ui messages and etc.

--- src/store/Reducers/bannerReducer.js
+++ src/store/Reducers/bannerReducer.js
@@ -1,6 +1,10 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import api from '../../api/api';
 
+// Utility function to handle errors
+const handleError = (error) =>
+  error.response?.data || { error: 'An error occurred' };
+
 export const add_banner = createAsyncThunk(
   'banner/add_banner',
   async (info, { rejectWithValue, fulfillWithValue }) => {
@@ -11,12 +15,40 @@ export const add_banner = createAsyncThunk(
 
       return fulfillWithValue(data);
     } catch (error) {
-      return rejectWithValue(
-        error.response?.data || { error: 'An error occurred' }
-      );
+      return rejectWithValue(handleError(error));
     }
   }
-); // End of get Vendor method.
+); // End of add banner by product id method.
+
+export const get_banner = createAsyncThunk(
+  'banner/get_banner',
+  async (productId, { rejectWithValue, fulfillWithValue }) => {
+    try {
+      const { data } = await api.get(`/get/banner/${productId}`, {
+        withCredentials: true,
+      });
+
+      return fulfillWithValue(data);
+    } catch (error) {
+      return rejectWithValue(handleError(error));
+    }
+  }
+); // End of get banner by product id method.
+
+export const update_banner = createAsyncThunk(
+  'banner/update_banner',
+  async ({ bannerId, info }, { rejectWithValue, fulfillWithValue }) => {
+    try {
+      const { data } = await api.put(`/update/banner/${bannerId}`, info, {
+        withCredentials: true,
+      });
+
+      return fulfillWithValue(data);
+    } catch (error) {
+      return rejectWithValue(handleError(error));
+    }
+  }
+); // End of update banner by product id method.
 
 const initialState = {
   successMessage: '',
@@ -30,24 +62,33 @@ export const bannerReducer = createSlice({
   name: 'banner',
   initialState,
   reducers: {
-    clearMessages: (state, _) => {
+    clearMessages: (state) => {
       state.successMessage = '';
       state.errorMessage = '';
     },
   },
   extraReducers: (builder) => {
+    const handlePending = (state) => {
+      state.loader = true;
+      state.errorMessage = '';
+    };
+    const handleFulfilled = (state, { payload }) => {
+      state.loader = false;
+      state.banner = payload.banner;
+      state.successMessage = payload.message;
+    };
+    const handleRejected = (state, { payload }) => {
+      state.loader = false;
+      state.errorMessage = payload?.message || 'An error occurred';
+    };
     builder
-      .addCase(add_banner.pending, (state) => {
-        state.loader = true;
-      })
-      .addCase(add_banner.fulfilled, (state, { payload }) => {
-        state.loader = false;
-        state.successMessage = payload.message;
-      })
-      .addCase(add_banner.rejected, (state, { payload }) => {
-        state.loader = false;
-        state.errorMessage = payload.message || 'An error occurred';
-      });
+      .addCase(add_banner.pending, handlePending)
+      .addCase(add_banner.fulfilled, handleFulfilled)
+      .addCase(add_banner.rejected, handleRejected)
+      .addCase(get_banner.fulfilled, handleFulfilled)
+      .addCase(update_banner.pending, handlePending)
+      .addCase(update_banner.fulfilled, handleFulfilled)
+      .addCase(update_banner.rejected, handleRejected);
   },
 });
 
--- src/views/vendor/AddBanner.jsx
+++ src/views/vendor/AddBanner.jsx
@@ -1,20 +1,42 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import toast from 'react-hot-toast';
 
 import { FaRegImage } from 'react-icons/fa6';
 
 import { PropagateLoader } from 'react-spinners';
 import { overrideStyle } from '../../utils/utils';
-import { add_banner } from '../../store/Reducers/bannerReducer';
+import {
+  clearMessages,
+  add_banner,
+  get_banner,
+  update_banner,
+} from '../../store/Reducers/bannerReducer';
 
 const AddBanner = () => {
   const dispatch = useDispatch();
   const { productId } = useParams();
-  const loader = false;
 
   const [showImage, setShowImage] = useState('');
   const [image, setImage] = useState('');
+
+  const { loader, successMessage, errorMessage, banner } = useSelector(
+    (state) => state.banner
+  );
+
+  useEffect(() => {
+    if (successMessage) {
+      toast.success(successMessage);
+      dispatch(clearMessages());
+      setShowImage('');
+      setImage('');
+    }
+    if (errorMessage) {
+      toast.error(errorMessage);
+      dispatch(clearMessages());
+    }
+  }, [dispatch, successMessage, errorMessage]);
 
   const imageHandler = (e) => {
     const files = e.target.files;
@@ -26,7 +48,7 @@ const AddBanner = () => {
     }
   }; // End of image handler
 
-  const addBannerImageForm = (e) => {
+  const addBannerImage = (e) => {
     e.preventDefault();
     const formData = new FormData();
     formData.append('productId', productId);
@@ -34,61 +56,144 @@ const AddBanner = () => {
     dispatch(add_banner(formData));
   }; // End of add banner image form
 
+  const updateBannerImage = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('mainBanner', image);
+    dispatch(update_banner({ info: formData, bannerId: banner._id }));
+  }; // End of update banner image form
+
+  useEffect(() => {
+    dispatch(get_banner(productId));
+  }, [dispatch, productId]);
+
   return (
     <div className='px-2 lg:px-7 pt-5'>
       <h1 className='text-[20px] font-bold mb-3 text-indigo-700'>Add Banner</h1>
       <div className='w-full p-4 bg-[#6a5fdf] rounded-md'>
-        <form onSubmit={addBannerImageForm}>
-          <div className='mb-4'>
-            <label
-              className='flex justify-center items-center flex-col h-[180px] cursor-pointer border border-dashed  hover:border-[#3f34b8]  w-full rounded-lg text-white'
-              htmlFor='image'
-            >
-              <span className='text-4xl'>
-                <FaRegImage size={25} />
-              </span>
-              <span>Select banner image</span>
-            </label>
-            <input
-              required
-              onChange={imageHandler}
-              className='hidden '
-              type='file'
-              id='image'
-            />
-          </div>
+        {!banner ? (
+          <div>
+            <form onSubmit={addBannerImage}>
+              <div className='mb-4'>
+                <label
+                  className='flex justify-center items-center flex-col h-[180px] cursor-pointer border border-dashed  hover:border-[#3f34b8]  w-full rounded-lg text-white'
+                  htmlFor='image'
+                >
+                  <span className='text-4xl'>
+                    <FaRegImage size={25} />
+                  </span>
+                  <span>Select banner image</span>
+                </label>
+                <input
+                  required
+                  onChange={imageHandler}
+                  className='hidden '
+                  type='file'
+                  id='image'
+                />
+              </div>
 
-          {showImage && (
-            <div>
-              <img
-                className='w-full h-[350px] object-contain rounded-lg shadow-lg'
-                src={showImage}
-                alt='Uploaded img'
-              />
-            </div>
-          )}
-          <button
-            disabled={loader ? true : false}
-            type='submit'
-            className={`bg-indigo-600 text-white rounded-lg py-2 px-7 my-2
+              {showImage && (
+                <div>
+                  <img
+                    className='w-full h-[350px] object-contain rounded-lg shadow-lg'
+                    src={showImage}
+                    alt='Uploaded img'
+                  />
+                </div>
+              )}
+              <button
+                disabled={loader ? true : false}
+                type='submit'
+                className={`bg-indigo-600 text-white rounded-lg py-2 px-7 my-2
     ${
       loader
         ? 'cursor-not-allowed pointer-events-none'
         : 'hover:bg-indigo-400 hover:shadow-md hover:shadow-indigo-400/40 cursor-pointer'
     }`}
-          >
-            {loader ? (
-              <div
-                className='bg-indigo-600
-                     rounded-lg w-36 px-7 '
               >
-                <PropagateLoader color='#D1D5DB' cssOverride={overrideStyle} />
+                {loader ? (
+                  <div
+                    className='bg-indigo-600
+                     rounded-lg w-36 px-7 '
+                  >
+                    <PropagateLoader
+                      color='#D1D5DB'
+                      cssOverride={overrideStyle}
+                    />
+                  </div>
+                ) : (
+                  'Add banner'
+                )}
+              </button>
+            </form>
+          </div>
+        ) : (
+          <div>
+            <div>
+              <img
+                className='w-full h-[350px] object-contain rounded-lg shadow-lg'
+                src={banner.banner}
+                alt='Existing banner img'
+              />
+            </div>
+
+            <form onSubmit={updateBannerImage}>
+              <div className='mb-4'>
+                <label
+                  className='flex justify-center items-center flex-col h-[180px] cursor-pointer border border-dashed  hover:border-[#3f34b8]  w-full rounded-lg text-white'
+                  htmlFor='image'
+                >
+                  <span className='text-4xl'>
+                    <FaRegImage size={25} />
+                  </span>
+                  <span>Select banner image</span>
+                </label>
+                <input
+                  required
+                  onChange={imageHandler}
+                  className='hidden '
+                  type='file'
+                  id='image'
+                />
               </div>
-            ) : (
-              'Add banner'
-            )}
-          </button>
-        </form>
+
+              {showImage && (
+                <div>
+                  <img
+                    className='w-full h-[350px] object-contain rounded-lg shadow-lg'
+                    src={showImage}
+                    alt='Uploaded img'
+                  />
+                </div>
+              )}
+              <button
+                disabled={loader ? true : false}
+                type='submit'
+                className={`bg-indigo-600 text-white rounded-lg py-2 px-7 my-2
+    ${
+      loader
+        ? 'cursor-not-allowed pointer-events-none'
+        : 'hover:bg-indigo-400 hover:shadow-md hover:shadow-indigo-400/40 cursor-pointer'
+    }`}
+              >
+                {loader ? (
+                  <div
+                    className='bg-indigo-600
+                     rounded-lg w-36 px-7 '
+                  >
+                    <PropagateLoader
+                      color='#D1D5DB'
+                      cssOverride={overrideStyle}
+                    />
+                  </div>
+                ) : (
+                  'Update banner'
+                )}
+              </button>
+            </form>
+          </div>
+        )}
       </div>
     </div>
   );
